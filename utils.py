b0e2399a147be7d315d9319d73948a7cf1b019d6
Sinavet <zerefdparhnjl@gmail.com>
2025-07-10 01:30:01 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,5 @@
-import os
-try:
-    import toml
-    config = toml.load(os.path.join(os.path.dirname(__file__), '../ReconT/.streamlit/config.toml'))
-    SUPPORTED_EXTS = tuple(config.get('supported_exts', ['.jpg', '.jpeg', '.png', '.bmp', '.webp', '.tiff', '.heic', '.heif']))
-    MAX_SIZE_MB = int(config.get('max_size_mb', 300))
-except Exception:
-    SUPPORTED_EXTS = ('.jpg', '.jpeg', '.png', '.bmp', '.webp', '.tiff', '.heic', '.heif')
-    MAX_SIZE_MB = 300
+SUPPORTED_EXTS = ('.jpg', '.jpeg', '.png', '.bmp', '.webp', '.tiff', '.heic', '.heif')
+MAX_SIZE_MB = 300
 MAX_SIZE_BYTES = MAX_SIZE_MB * 1024 * 1024
 
 def filter_large_files(uploaded_files, st=None):
